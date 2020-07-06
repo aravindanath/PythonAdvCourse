52ec48879bffd7768dd2e881c6e6c41f57ff957b
aravindanathdm <aravindanathdm@wibmo.com>
2020-07-06 22:03:22 +0530

    completion

--- Assignment/AssignJuly1.py
+++ Assignment/AssignJuly1.py
@@ -1,12 +1,15 @@
 #how to print class attribute value,
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 import time
 import pytest
 
 #def launchBrowser():
 
-driver = webdriver.Chrome(executable_path="D:\\Drivers\\chromedriver.exe")
+# driver = webdriver.Chrome(executable_path="D:\\Drivers\\chromedriver.exe")
+driver = webdriver.Chrome(ChromeDriverManager().install())
 Url = "https://www.mysmartprice.com/mobile/apple-iphone-xir-msp15688"
+
 driver.get(Url)
 driver.maximize_window()
 mobileList = driver.find_elements_by_xpath("//div[@class='prc-box prc-tbl--fullcards']/div")
@@ -40,10 +43,11 @@ for eachwin in childWindow:
 
 #amazon---
 global ama_price
-global flipkartLnk
+global flp_Price
 print(driver.title)
 time.sleep(5)
 ama_price = driver.find_element_by_xpath("//span[@id='priceblock_ourprice']").text
+finalAMz= str(ama_price).replace("₹ ","").replace(",","").split(".")[0]
 print("amazon price:"+ama_price)
 if ama_price >= mobilePrice[0]:
     print("Amazon website price is greater")
@@ -52,19 +56,40 @@ else:
 driver.close()
 driver.switch_to.window(parentWin)
 time.sleep(4)
-
+print(driver.title)
 #Flipkart------
 flipkartLnk = driver.find_element_by_xpath("//div[@class='prc-box prc-tbl--fullcards']/div[2]/div/div[3]/a")
 flipkartLnk.click()
 time.sleep(4)
-for eachwin in childWindow:
-    driver.switch_to.window(eachwin)
+
+childWindow1= driver.window_handles[1]
+print(childWindow)
+print(childWindow1)
+driver.switch_to.window(childWindow1)
+
 
 # driver.switch_to.window(amazon_win)
-flp_Price = driver.find_element_by_xpath("//div[@id='container']/div/div[3]/div[2]/div[1]/div[2]/div[2]/div/div[3]/div[1]/div/div").text
+time.sleep(2)
+flp_Price = driver.find_element_by_xpath("//div[contains(text(),'₹')]").text
+finalFpkt = str(flp_Price).replace("₹","").replace(",","").split(".")[0]
 print("flipkart price:"+flp_Price)
 driver.close()
 driver.switch_to.window(parentWin)
+
+
+
+price = {}
+
+price["Amazon"]=finalAMz
+price["Flipkart"]=finalFpkt
+
+for finalPrice in sorted(price.items()):
+    print(finalPrice)
+
+
+
+
+driver.quit()
 
 
 
