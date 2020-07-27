4675cc8625277f4fa72c997c86d3886a35732685
aravindanathdm <aravindanathdm@wibmo.com>
2020-07-27 22:09:39 +0530

    assignmet broken

--- brokenlinks/brokenlinks.py
+++ brokenlinks/brokenlinks.py
@@ -5,10 +5,15 @@ import requests
 driver = Chrome(ChromeDriverManager().install())
 driver.get("http://www.deadlinkcity.com/")
 links = driver.find_elements_by_tag_name("a")
-r =  200
-for link in links:
-    r = requests.head(link.get_attribute("href"))
-    if r.status_code >=400:
-        print(str(link.get_attribute('href')) + " is broken link ")
-    elif r.status_code ==200:
-        print(str(link.get_attribute('href')) + " is  available.")
+try:
+    r = 200
+    for link in links:
+        r = requests.head(link.get_attribute("href"))
+        if r.status_code >= 400:
+            print(str(link.get_attribute('href')) + " is broken link ")
+        elif r.status_code == 200:
+            print(str(link.get_attribute('href')) + " is  available.")
+except:
+    print("exception")
+finally:
+    driver.quit()
